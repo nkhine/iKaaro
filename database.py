2743dc8387914bd405d020b864259238f159f5e3
J. David Ibanez <jdavid@itaapy.com>
2009-08-28 19:21:02 +0200

    Move {add,change,remove}_resource to the context

--- database.py
+++ database.py
@@ -86,44 +86,6 @@ class Database(ReadOnlyDatabase, GitDatabase):
         path = '%s/database' % target
         GitDatabase.__init__(self, path, size_min, size_max)
 
-        # The resources that been added, removed, changed and moved can be
-        # represented as a set of two element tuples.  But we implement this
-        # with two dictionaries (old2new/new2old), to be able to access any
-        # "tuple" by either value.  With the empty tuple we represent the
-        # absence of change.
-        #
-        #  Tuple        Description                Implementation
-        #  -----------  -------------------------  -------------------
-        #  ()           nothing has been done yet  {}/{}
-        #  (None, 'b')  resource 'b' added         {}/{'b':None}
-        #  ('b', None)  resource 'b' removed       {'b':None}/{}
-        #  ('b', 'b')   resource 'b' changed       {'b':'b'}/{'b':'b'}
-        #  ('b', 'c')   resource 'b' moved to 'c'  {'b':'c'}/{'c':'b'}
-        #
-        # In real life, every value is either None or an absolute path (as a
-        # byte stringi).  For the description that follows, we use the tuples
-        # as a compact representation.
-        #
-        # There are four operations:
-        #
-        #  A(b)   - add "b"
-        #  R(b)   - remove "b"
-        #  C(b)   - change "b"
-        #  M(b,c) - move "b" to "c"
-        #
-        # Then, the algebra is:
-        #
-        # ()        -> A(b) -> (None, 'b')
-        # (b, None) -> A(b) -> (b, b)
-        # (None, b) -> A(b) -> error
-        # (b, b)    -> A(b) -> error
-        # (b, c)    -> A(b) -> (b, b), (None, c) FIXME Is this correct?
-        #
-        # TODO Finish
-        #
-        self.resources_old2new = {}
-        self.resources_new2old = {}
-
 
     @lazy
     def catalog(self):
@@ -138,126 +100,6 @@ class Database(ReadOnlyDatabase, GitDatabase):
         # The git cache only works on read-only mode
         revisions = self.get_revisions(files, 1)
         return revisions[0] if revisions else None
-
-
-    #######################################################################
-    # Events API
-    #######################################################################
-    def remove_resource(self, resource):
-        old2new = self.resources_old2new
-        new2old = self.resources_new2old
-
-        if isinstance(resource, Folder):
-            for x in resource.traverse_resources():
-                path = str(x.get_canonical_path())
-                old2new[path] = None
-                new2old.pop(path, None)
-        else:
-            path = str(resource.get_canonical_path())
-            old2new[path] = None
-            new2old.pop(path, None)
-
-
-    def add_resource(self, resource):
-        old2new = self.resources_old2new
-        new2old = self.resources_new2old
-
-        # Catalog
-        if isinstance(resource, Folder):
-            for x in resource.traverse_resources():
-                path = str(x.get_canonical_path())
-                new2old[path] = None
-        else:
-            path = str(resource.get_canonical_path())
-            new2old[path] = None
-
-
-    def change_resource(self, resource):
-        old2new = self.resources_old2new
-        new2old = self.resources_new2old
-
-        path = str(resource.get_canonical_path())
-        if path in old2new and not old2new[path]:
-            raise ValueError, 'cannot change a resource that has been removed'
-
-        if path not in new2old:
-            old2new[path] = path
-            new2old[path] = path
-
-
-    def move_resource(self, source, new_path):
-        old2new = self.resources_old2new
-        new2old = self.resources_new2old
-
-        def f(source_path, target_path):
-            source_path = str(source_path)
-            target_path = str(target_path)
-
-            if source_path in old2new and not old2new[source_path]:
-                raise ValueError, 'cannot move a resource that has been removed'
-
-            source_path = new2old.pop(source_path, source_path)
-            if source_path:
-                old2new[source_path] = target_path
-            new2old[target_path] = source_path
-
-
-        old_path = source.get_canonical_path()
-        if isinstance(source, Folder):
-            for x in source.traverse_resources():
-                x_old_path = x.get_canonical_path()
-                x_new_path = new_path.resolve2(old_path.get_pathto(x_old_path))
-                f(x_old_path, x_new_path)
-        else:
-            f(old_path, new_path)
-
-
-    #######################################################################
-    # Transactions API
-    #######################################################################
-    def _before_commit(self):
-        context = get_context()
-        root = context.root
-
-        # 1. Update links when resources moved
-        for source, target in self.resources_old2new.items():
-            if target and source != target:
-                target = Path(target)
-                resource = root.get_resource(target)
-                resource._on_move_resource(source)
-
-        # 2. Documents to unindex (the update_links methods calls
-        # server.change_resource which may modify the resources_old2new
-        # dictionary)
-        docs_to_unindex = self.resources_old2new.keys()
-        self.resources_old2new.clear()
-
-        # 3. Index
-        git_date = context.timestamp
-        user = context.user
-        userid = user.name if user else None
-        docs_to_index = []
-        for path in self.resources_new2old:
-            resource = root.get_resource(path)
-            if git_date:
-                resource.metadata.set_property('mtime', git_date)
-                resource.metadata.set_property('last_author', userid)
-            values = resource.get_catalog_values()
-            docs_to_index.append((resource, values))
-        self.resources_new2old.clear()
-
-        # 4. Find out commit author & message
-        git_msg = 'no comment'
-        git_author = (
-            '%s <%s>' % (userid, user.get_property('email'))
-            if user else 'nobody <>')
-        git_msg = getattr(context, 'git_message', None)
-        git_msg = (
-            git_msg.encode('utf-8')
-            if git_msg else "%s %s" % (context.method, context.uri))
-
-        # Ok
-        return git_author, git_date, git_msg, docs_to_index, docs_to_unindex
 
 
     def _save_changes(self, data):
@@ -277,13 +119,7 @@ class Database(ReadOnlyDatabase, GitDatabase):
 
     def _abort_changes(self):
         GitDatabase._abort_changes(self)
-
-        # Catalog
         self.catalog.abort_changes()
-
-        # Clear events
-        self.resources_old2new.clear()
-        self.resources_new2old.clear()
 
 
 
